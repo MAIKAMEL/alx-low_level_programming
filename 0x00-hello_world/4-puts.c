ed83437ad016a07b0423fa6c10e7f1c52c61f459
MAIKAMEL <maikamel945@gmail.com>
2023-08-11 10:50:21 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#include <stdio.h>/** * main- A program that prints a line* return 0 (success)*/int main(void){puts("\"Programming is like building a multilingual puzzle");return (0);}
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
