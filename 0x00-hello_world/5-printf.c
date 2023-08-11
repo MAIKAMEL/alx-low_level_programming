0c5b85aacf852ab719f9295f15ac2d5d39a3323b
MAIKAMEL <maikamel945@gmail.com>
2023-08-11 11:35:24 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this function for task 5
+ * Return: return(0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
