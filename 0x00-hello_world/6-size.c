a126449e801531797091dab792714de3f3642f0a
MAIKAMEL <maikamel945@gmail.com>
2023-08-11 11:53:52 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - the function for task 6
+ * Return: return(0)
+  */
+
+int main(void)
+{
+	int i;
+	char c;
+	double d;
+	printf("size of int : %lu.\n", (unsigned long)sizeof(i));
+	printf("size of char : %lu.\n", (unsigned long)sizeof(c));
+	printf("size of double : %lu.\n", (unsigned long)sizeof(d));
+	return (0);
+}
