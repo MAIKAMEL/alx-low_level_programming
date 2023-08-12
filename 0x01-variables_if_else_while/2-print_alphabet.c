57994d256fca1b07084a61e11ae8eaff2c3f18b3
MAIKAMEL <maikamel945@gmail.com>
2023-08-12 04:08:33 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - get the alphapet lowercase
+ * Return: return(0)
+ */
+int main(void)
+{
+	for (int i = 97; i < 122; i++)
+	{
+		printf("%c", i);
+	}
+	return (0);
+}
