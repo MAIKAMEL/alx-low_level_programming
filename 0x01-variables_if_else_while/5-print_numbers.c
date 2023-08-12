83ffa0b723f4bc614d9c4c02d17cd38771b967ef
MAIKAMEL <maikamel945@gmail.com>
2023-08-12 05:25:53 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print numbers
+ * Return: return(0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf(i);
+	}
+	return (0);
+}
