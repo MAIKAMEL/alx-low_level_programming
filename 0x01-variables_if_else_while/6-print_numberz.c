7e75707d56ffd45308c6d7ba3417f3324d3e129d
MAIKAMEL <maikamel945@gmail.com>
2023-08-12 05:33:37 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * Return: return(0)
+ */
+int main(void)
+{
+	int digit = '0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
