ec0a8952614a1b2aa878cbc7b97081cc05a72cb1
MAIKAMEL <maikamel945@gmail.com>
2023-08-12 04:28:14 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description:
+ * Return: return 0
+ *
+ */
+int main(void)
+{
+	char lower = 'a', upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar("\n");
+	return (0);
+}
