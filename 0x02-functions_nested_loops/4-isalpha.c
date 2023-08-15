3c15cc12a806ddbf1d7a689e078614cbf96167d0
MAIKAMEL <maikamel945@gmail.com>
2023-08-15 16:05:19 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * _islower - Entry point
+ * _isalpha - Entry point
  * Description: print all lowercase alphabit except e and q
  * @c: it is an input
  * Return: return(0)
  */
-int _islower(int c)
+int _isalpha(int c)
 {
         if (c >= 97 && c <= 122)
-	{
-		if (c >= 65 && c <= 90)
-			return (1);
-	}
-        else
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
 		return (0);
 }
