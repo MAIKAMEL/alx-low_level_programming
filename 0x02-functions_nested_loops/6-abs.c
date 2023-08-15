bb3f4d9e9e177e603e890f95121a1ff386232a2e
MAIKAMEL <maikamel945@gmail.com>
2023-08-15 16:33:22 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @int: it is an input
+ * Return: return(0)
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = (-1) * int;
+	}
+	return (int);
+}
