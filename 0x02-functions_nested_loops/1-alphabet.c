9f87400c05b1c9e2f13b430eec27ce642ff6ca32
MAIKAMEL <maikamel945@gmail.com>
2023-08-15 15:39:40 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ * main - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * Return: return(0)
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	printf("\n");
+}
