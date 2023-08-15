3557a13366cd02b9ea56bb0e165ec55f8d46f88f
MAIKAMEL <maikamel945@gmail.com>
2023-08-15 15:56:09 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * Return: return(0)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return(1);
+	else
+		return (0);
+}
