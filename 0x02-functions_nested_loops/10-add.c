ebf304c74e79883b05c4b1e5e549bdd83d21a947
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 15:49:33 -0700

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+printf("\n");
+
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @a: it is an input
+ * @a: it is an input
+ * Return: return(0)
+ */
+int add(int, int)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
