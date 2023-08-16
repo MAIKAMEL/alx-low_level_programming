b2c27ada7d2a201cf36c1d39ce72bf5a83d68d1e
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 15:52:18 -0700

    task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - Entry point
  * Description: print all lowercase alphabit except e and q
  * @a: it is an input
- * @a: it is an input
+ * @b: it is an input
  * Return: return(0)
  */
 int add(int a, int b)
