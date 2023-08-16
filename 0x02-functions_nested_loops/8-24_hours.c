1b7f0f883d8973dec1df92f97330084c99b20088
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 15:26:45 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * _islower - Entry point
+ * jack_bauer - Entry point
  * Description: print all lowercase alphabit except e and q
- * @c: it is an input
  * Return: return(0)
  */
 void jack_bauer(void)
@@ -11,8 +10,6 @@ void jack_bauer(void)
 
 	for (h = 0; h < 23; h++)
 	{
-		h = h / 10;
-		h = h % 10;
 		for (m = 0; m < 95; m++)
 		{
 			_putchar((h / 10) + 48);
