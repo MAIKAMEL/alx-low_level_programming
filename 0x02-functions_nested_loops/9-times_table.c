81e21c01aed964a177938f7c0f7be746555534fa
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 15:46:16 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (mul = 1; mul <=9; mul++)
+		for (mul = 1; mul <= 9; mul++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -20,8 +20,8 @@ void times_table(void)
 			if (pro <= 9)
 				_putchar(' ');
 			else
-				_putchar((pro / 10 ) + 48);
-			_putchar((pro % 10 ) + 48);
+				_putchar((pro / 10) + 48);
+			_putchar((pro % 10) + 48);
 		}
 		_putchar('\n');
 	}
