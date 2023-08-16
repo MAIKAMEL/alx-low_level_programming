c4edb7499359fe05297989e6a881d59b758b22bc
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 15:38:16 -0700

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * Return: return(0)
+ */
+void times_table(void)
+{
+	int num, mul, pro;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (mul = 1; mul <=9 mul++0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			pro = num * mul;
+
+			if (pro <= 9)
+				_putchar(' ');
+			else
+				_putchar((pro / 10 ) + 48);
+			_putchar((pro % 10 ) + 48);
+		}
+		_putchar('\n');
+	}
+}
