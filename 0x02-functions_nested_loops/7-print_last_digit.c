627cb3470a5243e0c9680fb3dbe72f53ac259027
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 05:54:53 -0700

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,10 @@ int print_last_digit(int n)
 {
 	int digit;
 
-	digit = n % 10;
-	_putchar(digit);
-	_putchar('\n');
-
-	return (0);
+	if (n < 0)
+		digit = (-1) * (n % 10);
+	else
+		digit = n % 10;
+	_putchar(digit + '0');
+	return (digit);
 }
