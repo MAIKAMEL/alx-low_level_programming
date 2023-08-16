f823d62cfbe5dd465063be48f9cd453daf762de3
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 15:39:34 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (mul = 1; mul <=9 mul++0)
+		for (mul = 1; mul <=9 mul++)
 		{
 			_putchar(',');
 			_putchar(' ');
