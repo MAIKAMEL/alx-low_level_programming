de4f66936d7c2c8e04fa26319f0324d1f07b9ba6
MAIKAMEL <maikamel945@gmail.com>
2023-08-16 15:58:04 -0700

    task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @n: it is an input
+ * Return: return(0)
+ */
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for(count = n; count > 98; count--)
+			printf("%d, ", count);
+	else
+		for(count = n; count < 98; count++)
+			printf("%d, ", count);
+	printf("98\n");
+}
