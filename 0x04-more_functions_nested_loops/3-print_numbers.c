51bf5eb4ef88f7475275b8ae1cb1fc381698de7e
MAIKAMEL <maikamel945@gmail.com>
2023-08-17 16:02:09 -0700

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * Return: return(0)
+ */
+void print_numbers(void)
+{
+	int num;
+
+	do {
+		_putchar(num + 48)
+			num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
