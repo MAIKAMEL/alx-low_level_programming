942bc7b83ae4c6aa131ce38ad19ec93eb821ce3a
MAIKAMEL <maikamel945@gmail.com>
2023-08-17 15:41:22 -0700

    task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @c: it is an input
+ * Return: return(0)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' || c <= 'Z')
+	      return (1);
+	else
+	return (0);
+}	
