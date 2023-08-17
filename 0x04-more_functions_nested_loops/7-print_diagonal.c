747082fa7e8f86e06e5217a521e563a15071e5f6
MAIKAMEL <maikamel945@gmail.com>
2023-08-17 16:31:37 -0700

    task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @n: it is an input
+ * Return: return(0)
+ */
+void print_diagonal(int n)
+{
+	int post, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (post = 1; post <= n; post++)
+		{
+			for (space = 1; space <= post; space++)
+				_putchar(' ');
+			_putchar('92');
+			_putchar('\n');
+		}
+	}
+}
