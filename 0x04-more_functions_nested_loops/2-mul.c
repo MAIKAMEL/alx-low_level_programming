fba81a3078ce9d689398990bce08fc453400e6a0
MAIKAMEL <maikamel945@gmail.com>
2023-08-17 15:57:55 -0700

    task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
  * mul - Entry point
  * Description: print all lowercase alphabit except e and q
  * @a: it is an input
- * @a: it is an input
+ * @b: it is an input
  * Return: return(0)
  */
 int mul(int a, int b)
