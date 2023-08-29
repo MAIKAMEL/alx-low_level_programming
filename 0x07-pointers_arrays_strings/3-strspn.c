b6919dc446ebb0e636294cdfeefc62d03afbc1c6
MAIKAMEL <maikamel945@gmail.com>
2023-08-29 21:29:50 +0300

    task

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn( - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @s: it is an input
+ * @accept: it is an input
+ * Return: return(0)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[i] !=  s[i]; j++)
+		{
+			if (accept[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (i);
+}
