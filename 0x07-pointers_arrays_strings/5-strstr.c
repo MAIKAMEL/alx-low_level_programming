6f84410f1ed9705c5df160cb3da72b6a58a5a7ab
MAIKAMEL <maikamel945@gmail.com>
2023-08-29 21:53:33 +0300

    task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strstr - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @haystack,: it is an input
+ * @needle: it is an input
+ * Return: return(0)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
