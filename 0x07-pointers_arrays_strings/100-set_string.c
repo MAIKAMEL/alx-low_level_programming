0ce949ea97864bdb5c8ef199cdbfe80b2918da3c
MAIKAMEL <maikamel945@gmail.com>
2023-08-29 22:58:18 +0300

    task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @s: it is an input
+ * @to: it is an input
+ * Return: return(0)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
