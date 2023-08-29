8572a0dd0612ae25a7c90e033e5cf62b414a3b59
MAIKAMEL <maikamel945@gmail.com>
2023-08-29 21:16:20 +0300

    task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - Entry point
+ * Description: print all lowercase alphabit except e and q
+ * @dest: it is an input
+ * @src: it is an input
+ * @n: it is an input
+ * Return: return(0)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
