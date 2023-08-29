8c79e1f5e24e0d17adb18cf10fb8eac188f57353
MAIKAMEL <maikamel945@gmail.com>
2023-08-29 22:53:32 +0300

    task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_diagsums - Entry point
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		s1 = s1 + a[(i * size) + i];
+		s1 = s1 + a[i * size + i];
 	}
 	for (i = size - 1; i >= 0; i--)
 	{
