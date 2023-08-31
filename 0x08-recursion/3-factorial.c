b25d625cd579977fc56f1d988f754ae328a2b625
MAIKAMEL <maikamel945@gmail.com>
2023-08-31 20:25:03 +0300

    task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * Description:pinter to string
+ * @c: it is an input
+ * Return: int n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
