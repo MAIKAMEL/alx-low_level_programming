10098e7f7533f007c6b8b39bdcca689857ecede0
MAIKAMEL <maikamel945@gmail.com>
2023-09-02 14:50:32 +0300

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ * @argc int
+ * @argv: list
+ * Return: return(0)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
