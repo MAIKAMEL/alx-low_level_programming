364945e96db044e0b334e9e72ab7c8efed5102b1
MAIKAMEL <maikamel945@gmail.com>
2023-09-04 15:24:10 +0300

    task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,8 +5,6 @@
 #include <stdio.h>
 
 char _putchar(char c);
-int _strlen(char *s)
-
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
