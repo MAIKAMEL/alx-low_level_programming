d73212491db8f722720c5590e34ae39f1eaea11f
MAIKAMEL <maikamel945@gmail.com>
2023-09-04 14:41:01 +0300

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *create_array: creates an array of char
+ * @size: size of array
+ * @c: char to inintialize
+ * Return: pointer to array or null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+
+	if (size == 0 || n == 0)
+		return (0);
+	while (size--)
+		n[size] = c;
+	return (n);
+}
