3b707d6f8b83c7dfbb520f0eb6fd9dc95adc53ad
MAIKAMEL <maikamel945@gmail.com>
2023-09-11 18:30:02 +0300

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog struct
+ * @name : dog's name
+ * @age  :dog's age
+ * @owmer :owner's name
+ * DEscription : dog struct in a big kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;'
+	char *owner;
+}
+
+#endif
