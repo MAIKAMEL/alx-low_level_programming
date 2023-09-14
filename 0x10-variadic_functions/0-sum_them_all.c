3137cf6e188183196081831093da0d8336d81600
MAIKAMEL <maikamel945@gmail.com>
2023-09-14 16:58:00 +0300

    Variadic functions task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum varaibles arguments
+ * @n: number of argumnet
+ * @...: the in teger to sum
+ * Return: sum of integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0, i = n;
+
+	va_list = ap;
+
+	if (!n)
+		return (0);
+	va_start(ap, n);
+	while (i--)
+		s += va_arg(ap, int);
+	va_end(ap);
+	return (s);
+}
