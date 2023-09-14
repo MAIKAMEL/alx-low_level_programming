6ed88d2de06252af9b1e08df306143c0cb49ed9e
MAIKAMEL <maikamel945@gmail.com>
2023-09-14 17:11:11 +0300

    Variadic functions task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - sum varaibles arguments
@@ -8,15 +9,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int s = 0, i = n;
-
 	va_list = ap;
+	unsigned in ti, sum = 0;
 
-	if (!n)
-		return (0);
 	va_start(ap, n);
-	while (i--)
-		s += va_arg(ap, int);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
 	va_end(ap);
-	return (s);
+
+	return (sum);
 }
